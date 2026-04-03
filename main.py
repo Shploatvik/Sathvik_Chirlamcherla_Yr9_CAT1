4ea586ed71845f0a907dad8ae7d6ca4d17dc7160
Sathvik C <sathvik.c8@outlook.com>
2026-04-03 14:16:08 +0800

    ok just to explain no i finished making a code where the user can type the formula into the code and that will create a graph in matplot lib using the formula. However, there is still more to do.
    the next step would be to allow the user to choose the amount of intervals to estimate the area underneath the curve, and I think that I could do this concurrently with the three ways of measuring the area under the curve. At this rate, I might be able to complete this today cause it is just that one thing, accuracy and YAY
    
    Also after drawing the rectangles using the methods, i will have to calculate the area of each rectangle and add it up. Since it would be most beneficial to store the values of each rectangle in a list, i feel like maybe matrix multiplication might have to done or smth and then I will have to sum it all up
    
    also to explain this code, the main part of the code is jsut this line - eval(formula, {"__builtins__": {}}, {**expressions, "x": x}).
    eval takes a string anf then turns it into python code. the builtin thingy basically makes sure that all builtin python functions are removes, so that no malicious input could be turned into python code such as imports and stuff. The expressions part of the code defines the only things that are allowed to be used. the double apostrophe basically unpaces the expressions dictionary and the x:x is put so that the function knows what x is.
    
    IM still at 0 followers why is no one following me?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# all the formulas allowed
 def get_function(formula):
     formula= formula.replace('^', '**')
     expressions = { 
@@ -12,13 +13,17 @@ def get_function(formula):
         return eval(formula, {"__builtins__": {}}, {**expressions, "x": x})
     return f
 
-lower_bound = int(input('What is the lower bound of x?'))
-upper_bound = int(input('What is the upper bound of x?'))
+# input variables
+lower_bound = int(input('What is the lower bound of x? '))
+upper_bound = int(input('What is the upper bound of x? '))
 formula = input("What formula is being used? (eg sin(x), x**2, etc.)")
-
+rectangles = int(input("How many rectangles do you want to use? "))
+method = input("What method do you want to use? (left, right, midpoint)")
+# x and y axis variables
 x = np.linspace(lower_bound, upper_bound, 400)
 y = get_function(formula)(x)
 
+# final graphing
 plt.plot(x, y, label=f"f(x) = {formula}")
 plt.grid(True)
 plt.legend()
