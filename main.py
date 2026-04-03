b5ab7170496beef04b0dff3c4d3d8fec59c087a3
Sathvik C <sathvik.c8@outlook.com>
2026-04-03 16:07:30 +0800

    so technically i finisihed my cat... Uh if you check the requirements: I asked the user to enter a function in the rterms of x which includes x to the power of any number and then adding or subtractinf a number to it. I also asked for both the upper and lower bound I asked how many rectangles to use I asked whether to use left end right end or centre rectangles I can also calculate the estimated area, though not with a lot of calculation demonstrated. Also, I only used a bit of ai, and I fully understand the code right now.
    Time to explain the code: So the major things i changed right now are the width thingy so now i can calculate the width of each rectangle after we get all the inputs. Then depending on the method required, i used linspace to find the same numbers of x values as the numbers of rectangles and changed where to find them as each one has a different location.
    
    Then using that, the f get function is used for the heights and then using both of these values, I drew a bar which basically represents the rectangles.
    
    Finally the final estimate of the area is calculated and printed out. However I will have to print out the calculations to make them more obvious. I did take help of ai, but I will say I understand every line of code here. YAY.
    
    Now the next steps are to improve the prints of the calculations and that should be fine. After that, I would like to use ai now that i proved i can do it myself. I want to:
    show the area of each rectangle on the rectangle on the graph when you click it
    make the input work even with words instead of symbols
    try put it into pygame and make it more cool and engaging
    make it look sick
    I also have to check if the area works when the graph goes underneath the x axis.
    
    BRO Y is no one following me im actually cooking rn!!!

--- main.py
+++ main.py
@@ -16,12 +16,29 @@ def get_function(formula):
 # input variables
 lower_bound = int(input('What is the lower bound of x? '))
 upper_bound = int(input('What is the upper bound of x? '))
-formula = input("What formula is being used? (eg sin(x), x**2, etc.)")
+formula = input("What formula is being used? (eg sin(x), x**2, etc.) ").lower()
 rectangles = int(input("How many rectangles do you want to use? "))
-method = input("What method do you want to use? (left, right, midpoint)")
+method = input("What method do you want to use? (left, right, midpoint) ").lower()
 # x and y axis variables
 x = np.linspace(lower_bound, upper_bound, 400)
 y = get_function(formula)(x)
+
+# rectangles
+width = (upper_bound - lower_bound) / rectangles
+if method == "left":
+    x_values = np.linspace(lower_bound, upper_bound - width, rectangles)
+elif method == 'right':
+    x_values = np.linspace(lower_bound + width, upper_bound, rectangles)
+elif method == 'midpoint':
+    x_values = np.linspace(lower_bound + width/2, upper_bound - width/2, rectangles)
+f = get_function(formula)
+heights = f(x_values)
+
+for x_val, height_val in zip(x_values, heights):
+    plt.bar(x_val, height_val, width=width, align='center', alpha=0.3, edgecolor='black')
+
+final_estimate = np.sum(heights * width)
+print(final_estimate)
 
 # final graphing
 plt.plot(x, y, label=f"f(x) = {formula}")
